2cbb01e2aa49d15e908caa5c83b0c8b9315d310e
Protyush2001 <deyprotyush@gmail.com>
2024-12-03 16:38:17 +0530

    Made changes

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.use(express.json());
 
 const corsOptions = {
     origin: ["http://localhost:5173"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 };
 app.use(cors(corsOptions));
 
